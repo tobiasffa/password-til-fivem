0eeec0f230a7c78b6cafdca7642f3ab95cb3e770
udvikler i FiveM <70888884+tobiasffa@users.noreply.github.com>
2022-06-16 17:31:26 +0200

    Add files via upload

---
+++ password/__resource.lua
@@ -0,0 +1,9 @@
+-- Leaked By: Leaking Hub | J. Snow | leakinghub.com
+
+resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
+
+author 'Puntherline'
+description 'Advanced whitelist / password script.'
+version '1.1'
+
+server_script 'server.lua'
